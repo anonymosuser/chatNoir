44675867eda5eff29f2568d762436a9679b55f77
Rudko Adam <rudko.adam.1@gmail.com>
2020-05-28 17:54:48 +0300

    catPath (in progress )

--- chatNoir.js
+++ chatNoir.js
@@ -24,6 +24,93 @@ function generateDefaultAcitveTiles() {
 }
 let activeTiles = generateDefaultAcitveTiles();
 
+function getMoves(catPosition) {
+    let catI = catPosition.i;
+    let catJ = catPosition.j;
+
+    let moves = [
+        { i: catI + 1, j: catJ },
+        { i: catI - 1, j: catJ },
+        { i: catI, j: catJ + 1 },
+        { i: catI, j: catJ - 1 }
+    ]
+    if (catJ % 2) {
+        moves.push({ i: catI - 1, j: catJ - 1 })
+        moves.push({ i: catI - 1, j: catJ + 1 })
+    } else {
+        moves.push({ i: catI + 1, j: catJ - 1 })
+        moves.push({ i: catI + 1, j: catJ + 1 })
+    }
+
+    return moves;
+}
+
+function generateCatPath() {
+    let catPath = [];
+    catPath[catPosition.i] = [];
+    catPath[catPosition.i][catPosition.j] = '0';
+    // // cats move
+    // let moves = getMoves(catPosition);
+
+    // moves = moves.filter(move => {
+    //     // if this square is take - move is not possible
+    //     let { i, j } = move;
+    //     if ((activeTiles[i] && activeTiles[i][j]) || (catPath[i] && catPath[i][j])) {
+    //         return false;
+    //     }
+    //     return true
+    // });
+    // moves.forEach(move => {
+    //     catPath[move.i] = catPath[move.i] || [];
+    //     catPath[move.i][move.j] = 1;
+    // });
+
+    function doit (moves, index) {
+        let allMoves = [];
+        moves.forEach(move => {
+            let newMoves = getMoves(move);
+    
+            newMoves = newMoves.filter(move => {
+                // if this square is take - move is not possible
+                let { i, j } = move;
+                if ((activeTiles[i] && activeTiles[i][j]) || (catPath[i] && catPath[i][j])) {
+                    return false;
+                }
+                return true
+            });
+    
+            newMoves.forEach(move => {
+                catPath[move.i] = catPath[move.i] || [];
+                catPath[move.i][move.j] = index;
+            });
+
+            allMoves = [...allMoves, ...newMoves];
+        })
+        return allMoves;
+    }
+
+    let m1 = doit([catPosition], 1);
+    let m2 = doit(m1, 2);
+    let m3 = doit(m2, 3);
+    let m4 = doit(m3, 4);
+    let m5 = doit(m4, 5);
+    let m6 = doit(m5, 6);
+
+
+    
+
+
+
+
+
+    // merge catPath and activeMoves
+
+    return catPath;
+}
+
+let catPath = generateCatPath();
+console.log('catpos', catPosition);
+
 function handleTileClick(i, j) {
     console.log(i, j);
     moveCount++;
@@ -104,9 +191,11 @@ const render = () => {
             } else if (clickHandler) {
                 activeClass += ' possible-move';
             }
-
+            let catPathIndex = (catPath[i] && catPath[i][j]) || '';
             renderHtml += `
-                <div onclick="${clickHandler}" class="tile ${activeClass}" style="left: ${x}px; top: ${y}px;"></div>
+                <div onclick="${clickHandler}" class="tile ${activeClass}" style="left: ${x}px; top: ${y}px;">
+                    <div class="cat-path-index "> ${catPathIndex}</div>
+                </div>
             `;
         }
     }
